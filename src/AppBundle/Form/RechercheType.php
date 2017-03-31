b85553a405ae3d77432ec0bf0c27bb9d26b8ea17
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2017-03-31 09:38:58 +0000

    Generation et mise en page de la classe Recherche

---
+++ src/AppBundle/Form/RechercheType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class RechercheType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('libelle', TextType::class, array(
+                'attr'  => array(
+                    'class' => 'form-control',
+                    'autocomplete'  => 'off',
+                    'placeholder' => "Recherche..."
+                ),
+                'label_attr'  => array(
+                  'class' => 'invisible hidden',
+                  'required' => false,
+                )
+          ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Recherche'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_recherche';
+    }
+
+
+}
